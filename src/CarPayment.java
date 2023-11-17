7945bf7760b0e9f8f3047171c361cb557f81609f
Rebeca Palmeira <rebeca.palmeira@gmail.com>
2023-11-17 20:32:29 -0300

    add resolution to second exercise

---
+++ src/CarPayment.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class CarPayment {
+    public static void main(String[] args) {
+        /*
+        Dado o valor de um carro, informar em quantas vezes ele pode ser parcelado, considerando que o valor mínimo da
+        parcela é R$ 1000,00
+
+        O valor do carro deve ser informado pelo usuário via Scanner
+         */
+
+        Scanner scanner = new Scanner(System.in);
+
+        double carPrice, installmentValue;
+        int financingTerm;
+
+        System.out.println("Informe o valor do carro que deseja adquirir: R$ ");
+        carPrice = scanner.nextDouble();
+
+        System.out.println("Qual valor aproximado deseja pagar de parcela, a partir de R$ 1000,00 (mil reais)?");
+        installmentValue = scanner.nextDouble();
+
+        financingTerm = (int) carPrice / (int)installmentValue;
+        installmentValue = carPrice / financingTerm;
+
+        System.out.printf("\nConsiderando o valor total do veículo e o valor desejado para parcela, o prazo de financiamento será de %d meses em parcelas fixas de R$ %.2f",
+                financingTerm, installmentValue
+            );
+
+    }
+}
