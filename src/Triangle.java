02c26ed3a7dc911c14e138c094dbe28b0a89a18e
Rebeca Palmeira <rebeca.palmeira@gmail.com>
2023-11-17 22:38:19 -0300

    resolution of third exercise - outside of DEADLINE

--- src/Triangle.java
+++ src/Triangle.java
@@ -2,6 +2,11 @@ import java.util.Scanner;
 
 public class Triangle {
     public static void main(String[] args) {
+        /*
+        Criei uma branch separada para publicar a resolução do terceiro exercício que foi concluída FORA DO PRAZO
+        A resolução PARCIAL dentro do prazo está no código publicado na main branch
+         */
+
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Informe um número ímpar entre 5 e 11:");
@@ -12,26 +17,23 @@ public class Triangle {
             number = scanner.nextInt();
         }
 
-        int startingSpaces = (number - 1) / 2;
-        String spaces = " ";
-        String symbol = "*";
-        int spacesInLine = startingSpaces;
+        int lines = (number - 1) / 2;
+        int spacesInLine = lines;
         int symbols = 1;
 
-        for(int numberOfLines = startingSpaces; numberOfLines > 0; numberOfLines--) {//4
+        for(int numberOfLines = lines; numberOfLines >= 0; numberOfLines--) {
             for(int numberOfSpaces = spacesInLine; numberOfSpaces > 0; numberOfSpaces--) {
-                System.out.print(spaces);
+                System.out.print(" ");
             }
 
-
-
-            for(int symbolsInLine = symbols; symbolsInLine <= symbols; symbolsInLine+=2) {
+            for(int numberOfSymbols = 0; numberOfSymbols < symbols; numberOfSymbols++) {
                 System.out.print("*");
             }
-            spacesInLine--;
 
+            spacesInLine--;
+            symbols+=2;
+            
             System.out.print("\n");
         }
-
     }
 }
